01f9b033e2a4641eacecd55e70526473d89d528b
grainne82 <81164115+grainne82@users.noreply.github.com>
2022-09-25 13:39:50 +0100

    js

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
-/*hamburger toggle*/
 
 const hamburgerMenu = document.getElementsByClassName('hamburger-menu')[0];
 const navLinks = document.getElementsByClassName('nav-container')[0];
 const sideNav = document.getElementsByClassName('sidenavContainer')[0];
 const article = document.getElementsByClassName('article')[0];
-const navToggle = document.querySelectorAll('.toggle-icon');
+const navToggle = document.getElementsByClassName('hamburger-menu')[0];
 
 hamburgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active')
     sideNav.classList.toggle('active')
     article.classList.toggle('active')
-    navToggle.forEach(_icon => {
-        _icon.classList.toggle('hidden')
+    navToggle.classList.toggle('active')
     })
+
+
+window.addEventListener("resize", () => {
+    if(document.body.clientWidth > 769) {
+       navLinks.classList.remove('active')
+       sideNav.classList.remove('active') 
+       article.classList.remove('active')
+       navToggle.classList.remove('active')
+    }    
 })
 
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
 
+var subdropdown = document.getElementsByClassName("sub-dropdown-btn");
+var i;
 
+for (i = 0; i < subdropdown.length; i++) {
+  subdropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var subdropdownContent = this.nextElementSibling;
+    if (subdropdownContent.style.display === "block") {
+      subdropdownContent.style.display = "none";
+    } else {
+      subdropdownContent.style.display = "block";
+    }
+  });
+}
+   
